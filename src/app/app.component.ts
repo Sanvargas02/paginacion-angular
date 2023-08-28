63da723e17e9501e9420ca962407a69b0168e51b
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-28 10:43:04 -0500

    paginacion lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pagination-library';
+
+  //Página donde estamos
+  page: number = 1;
+
+  // collection: any[] = someArrayOfThings;
+
+  //Colección de datos traidos desde el back a listar
+  collection: any[]  = [
+    {
+      id: 1,
+      title: 'La Carreta',
+      date: '02/04/2023'
+    },
+    {
+      id: 2,
+      title: 'Paragón Plaza',
+      date: '02/04/2022'
+    },
+    {
+      id: 3,
+      title: 'Carne a la Llanera',
+      date: '02/04/2021'
+    },
+    {
+      id: 4,
+      title: 'Otro restaurant',
+      date: '02/04/2020'
+    },
+    {
+      id: 5,
+      title: 'Ultima búsqueda',
+      date: '02/04/2019'
+    },
+    {
+      id: 6,
+      title: 'La Carreta',
+      date: '02/04/2023'
+    },
+    {
+      id: 7,
+      title: 'Paragón Plaza',
+      date: '02/04/2022'
+    },
+    {
+      id: 8,
+      title: 'Carne a la Llanera',
+      date: '02/04/2021'
+    },
+    {
+      id: 9,
+      title: 'Otro restaurant',
+      date: '02/04/2020'
+    },
+    {
+      id: 10,
+      title: 'Ultima búsqueda',
+      date: '02/04/2019'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {NgxPaginationModule} from 'ngx-pagination'; // <-- import the module
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
